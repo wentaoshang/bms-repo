64ca156cfe5540e2b8281902c4619c32287d0277
Wentao Shang <wentaoshang@gmail.com>
2015-01-08 16:04:29 -0800

    Fix test cases.

--- src/test-client.js
+++ src/test-client.js
@@ -1,35 +1,84 @@
 var ndn = require('ndn-js');
 var tsToBuffer = require('./timestamp.js').tsToBuffer;
 
-var onData = function(interest, data) {
-  console.log("Data received in callback.");
-  console.log('Name: ' + data.getName().toUri());
-  console.log('Content: ' + data.getContent().buf().toString());
-  face.close();
+function onData(interest, data) {
+  console.log('Data received in callback.');
+  console.log('Name: %s', data.getName().toUri());
+  console.log('Content: %s', data.getContent().buf().toString());
+  test_cases.runNextCase();
 };
 
-var onTimeout = function(interest) {
-  console.log("Interest time out.");
-  console.log('Interest name: ' + interest.getName().toUri());
-  face.close();
+function onTimeout(interest) {
+  console.log('Interest time out.');
+  console.log('Interest name: %s', interest.getName().toUri());
+  test_cases.runNextCase();
 };
 
 var face = new ndn.Face(new ndn.UnixTransport(),
 			new ndn.UnixTransport.ConnectionInfo('/tmp/nfd.sock'));
 
-var name = new ndn.Name("/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage");
-name.append(tsToBuffer(new Date('Thu Jan 08 2015 18:04:07 GMT-0800 (PST)')));
-console.log("Express name " + name.toUri());
-face.expressInterest(name, onData, onTimeout);
+var test_cases = {
+ index: 0,
+ cases:
+ [function () {
+      var name = new ndn.Name('/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage');
+      name.append(tsToBuffer(new Date('Thu Jan 08 2015 18:04:07 GMT-0800 (PST)')));
+      console.log('Request %s', name.toUri());
+      face.expressInterest(name, onData, onTimeout);
+    },
 
-name = new ndn.Name("/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage");
-console.log("Express name " + name.toUri());
-face.expressInterest(name, onData, onTimeout);
+   function () {
+     var name = new ndn.Name('/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage');
+     console.log('Request %s', name.toUri());
+     face.expressInterest(name, onData, onTimeout);
+   },
 
-name = new ndn.Name("/test/ucla.edu/bms/melnitz/data/studio1/electrical");
-console.log("Express name " + name.toUri());
-face.expressInterest(name, onData, onTimeout);
+   function () {
+     var name = new ndn.Name('/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage');
+     console.log('Request %s with child selector', name.toUri());
+     var template = new ndn.Interest();
+     template.setChildSelector(1);
+     face.expressInterest(name, template, onData, onTimeout);
+   },
 
-name = new ndn.Name("/test/ucla.edu/bms/melnitz");
-console.log("Express name " + name.toUri());
-face.expressInterest(name, onData, onTimeout);
+   function () {
+     var name = new ndn.Name('/test/ucla.edu/bms/melnitz/data/studio1/electrical/AH8/voltage');
+     console.log('Request %s with exclude filter', name.toUri());
+     var ts = tsToBuffer(new Date('Thu Jan 08 2015 18:04:13 GMT-0800 (PST)'));
+     var ts_component = new ndn.Name().append(ts);
+     var filter = new ndn.Exclude([ndn.Exclude.ANY, ts]);
+     var template = new ndn.Interest();
+     template.setChildSelector(0);
+     template.setExclude(filter);
+     face.expressInterest(name, template, onData, onTimeout);
+   },
+
+   function () {
+     var name = new ndn.Name('/test/ucla.edu/bms/melnitz/data/studio1/electrical');
+     console.log('Request %s', name.toUri());
+     face.expressInterest(name, onData, onTimeout);
+   },
+
+   function () {
+     var name = new ndn.Name('/test/ucla.edu/bms/melnitz');
+     console.log('Request %s', name.toUri());
+     face.expressInterest(name, onData, onTimeout);
+   },
+   ],
+
+ runNextCase: function () {
+    this.index++;
+    this.run();
+  },
+
+ run: function () {
+    if (this.index >= this.cases.length)
+      {
+	face.close();
+	return;
+      }
+    this.cases[this.index]();
+  }
+};
+
+test_cases.run();
